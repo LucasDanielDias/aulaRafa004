46d99342eb9752a9ec9984d8d6aee1972d5f037a
12115838 <lucaas.daniel2003@gmail.com>
2023-04-11 21:17:51 -0300

    feat: Faz desafio aula Rafa

--- src/Atividade/Cantina.java
+++ src/Atividade/Cantina.java
@@ -1,6 +1,7 @@
 package Atividade;
 
 import javax.swing.*;
+import java.util.InputMismatchException;
 
 public class Cantina {
     public static void main(String[] args) {
@@ -11,10 +12,14 @@ public class Cantina {
         NumeroPdido.Massa.valor = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite o valor"));
             if (i != 0) {
                  do {
+                     try{
                      Integer opcao = Integer.valueOf(JOptionPane.showInputDialog("fale qual são os ingredientes que você vai querer\n"
                           + "1-Peperone\n2-Bacon\n3-calabresa\n4-molho\n0-fechar"));
-                      NumeroPdido.carrinho.add(opcao);
-                      i=opcao;
+                     NumeroPdido.carrinho.add(opcao);
+                     i=opcao;
+                     }catch (NumberFormatException Pedido){
+                         JOptionPane.showMessageDialog(null,"Digite um numero");
+                     }
                   }while (i!=0);
             }
             Double X = NumeroPdido.calcularPedido(NumeroPdido.Massa.valor,NumeroPdido.carrinho.size());
